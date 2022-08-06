f3b921c2a07e1c8e56290116ccaa1de7229f8892
Karl Asklof <karl7@kth.se>
2022-08-06 20:50:46 +0200

    react-icons added

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import {
   Text,
 } from "@chakra-ui/react";
 import { isValidMotionProp, motion } from "framer-motion";
+import { MdStar } from "react-icons/md";
 
 const ChakraBox = chakra(motion.div, {
   shouldForwardProp: (prop) => isValidMotionProp(prop) || prop === "children",
@@ -20,41 +21,46 @@ function App() {
     <div className="App">
       <header>
         <Center h="100vh">
-          <Box p="5" maxW="320px">
-            <ChakraBox
-              animate={{
-                scale: [1, 2, 2, 1, 1],
-                rotate: [0, 0, 270, 270, 0],
-                borderRadius: ["20%", "20%", "50%", "50%", "20%"],
-              }}
-              // @ts-ignore
-              transition={{
-                duration: 3,
-                ease: "easeInOut",
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-            >
-              <Image
-                src={portrait}
-                alt={"Be dev portrait"}
-                boxSize="600px"
-                _hover={{ scale: 1.1 }}
-              />
-              <Flex align="baseline" mt={2}>
-                <Badge colorScheme="orange">BE-dev</Badge>
-                <Text
-                  ml={2}
-                  textTransform="uppercase"
-                  fontSize="sm"
-                  fontWeight="bold"
-                  color="orange.800"
-                >
-                  Semi &bull; Sweden
-                </Text>
-              </Flex>
-            </ChakraBox>
-          </Box>
+          <ChakraBox
+            maxW="320px"
+            animate={{
+              scale: [1, 2, 2, 1, 1],
+              rotate: [0, 0, 270, 270, 0],
+              borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+            }}
+            // @ts-ignore
+            transition={{
+              duration: 3,
+              ease: "easeInOut",
+              repeat: Infinity,
+              repeatType: "loop",
+            }}
+          >
+            <Image
+              src={portrait}
+              alt={"Be dev portrait"}
+              boxSize="600px"
+              _hover={{ scale: 1.1 }}
+            />
+            <Flex align="baseline" mt={2}>
+              <Badge colorScheme="orange">BE-dev</Badge>
+              <Text
+                ml={2}
+                textTransform="uppercase"
+                fontSize="sm"
+                fontWeight="bold"
+                color="orange.800"
+              >
+                Semi-senior &bull; Sweden
+              </Text>
+            </Flex>
+            <Flex mt={2} align="center">
+              <Box as={MdStar} color="orange.400" />
+              <Text ml={1} fontSize="sm">
+                <b>7/10</b>
+              </Text>
+            </Flex>
+          </ChakraBox>
         </Center>
       </header>
     </div>
